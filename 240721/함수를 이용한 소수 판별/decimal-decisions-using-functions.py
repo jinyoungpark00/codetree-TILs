f33d50fc4a2c2eb0c1b69dabdaa94c4a9a4efcce
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-07-21 11:51:56 +0900

    Title: 함수를 이용한 소수 판별, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240721/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -8,8 +8,10 @@ def prime_sum(a, b):
     return result
 
 def is_prime(i):
+    if i <= 1:
+        return False
     for j in range(2, i):
-        if i % j == 0 or i == 1:
+        if i % j == 0:
             return False
     return True
 
