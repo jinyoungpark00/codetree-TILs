4171fd5037cc37eefc8bdde290d25c64dc25b650
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-07-30 15:56:05 +0900

    Title: k번째로 신기한 문자열, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/k번째로 신기한 문자열/kth-special-string.py
+++ 240730/k번째로 신기한 문자열/kth-special-string.py
@@ -7,10 +7,18 @@ special_array = []
 for _ in range(n):
     array.append(input())
 
+def is_string_in_array(string):
+    for i in range(len(st)):
+        char_st = st[i]
+        char_string = string[i]
+        if char_st != char_string:
+            return False
+    return True
+
+
 for string in array:
-    if st in string:
+    if is_string_in_array(string):
         special_array.append(string)
 
 sorted_special_array = sorted(special_array)
-
 print(sorted_special_array[k - 1])
