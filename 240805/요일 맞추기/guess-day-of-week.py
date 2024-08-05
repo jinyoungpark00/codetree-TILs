13152c50743c61291aee3d50ab159124f4cce8e4
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-05 23:52:32 +0900

    Title: 요일 맞추기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/요일 맞추기/guess-day-of-week.py
+++ 240805/요일 맞추기/guess-day-of-week.py
@@ -5,11 +5,12 @@ week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 def check_count(mon, day):
     count = 0
+    mon -= 1
     while mon > 0:
         count += month[mon]
         mon -= 1
     count += day
     return count
 
-diff = check_count(m2, d2) - check_count(m1, d1)
+diff = (check_count(m2, d2) - check_count(m1, d1)) % 7
 print(week[diff])
