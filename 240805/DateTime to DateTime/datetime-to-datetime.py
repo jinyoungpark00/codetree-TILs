680f71ea21b5ff8fc621ac817e5cf1257a0b9447
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-05 23:27:40 +0900

    Title: DateTime to DateTime, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240805/DateTime to DateTime/datetime-to-datetime.py
+++ 240805/DateTime to DateTime/datetime-to-datetime.py
@@ -4,6 +4,11 @@ def cal_time(day, hour, minute):
     day -= 11
     hour -= 11
     minute -= 11
-    return day * 24 * 60 + hour * 60 + minute
+    result = day * 24 * 60 + hour * 60 + minute
+    
+    if result > 0:
+        return result
+    else:
+        return -1
 
 print(cal_time(a, b, c))
