a22b7bdca28e21c54a3dc29d4d61973cc6cb76fb
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-05 23:44:43 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240805/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,15 @@
+m1, d1, m2, d2 = map(int, input().split())
+month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+
+def check_count(mon, day):
+    count = 0
+    while mon > 0:
+        count += month[mon]
+        mon -= 1
+    count += day
+    return count
+
+diff = check_count(m2, d2) - check_count(m1, d1)
+print(week[diff])
