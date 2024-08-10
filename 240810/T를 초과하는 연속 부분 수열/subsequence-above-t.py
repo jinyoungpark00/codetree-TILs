0798d8bc9fbc7e24d7ad1639303ff73284a482e6
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-10 17:28:24 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240810/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -1,15 +1,14 @@
-n, k = map(int, input().split())
+n, t = map(int, input().split())
 array = list(map(int, input().split()))
 
-count = 1
-max_count = 1
+max_count = 0
+count = 0
+
 for i in range(n):
-    if i == 0 or array[i] < k or array[i - 1] >= array[i]:
-        count = 1
-    elif array[i - 1] > k:
+    if array[i] > t:
         count += 1
+        max_count = max(max_count, count)
     else:
-        count = 1
-    max_count = max(max_count, count)
+        count = 0
 
 print(max_count)
