c4853e3f2867a1918e30d186df9807a19b1f5035
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-11 13:52:21 +0900

    Title: 좌우로 움직이는 로봇, Time: 119ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 240811/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,36 @@
+MAX = 1000000
+
+n, m = map(int, input().split())
+robot_a = [0] * (MAX + 1)
+robot_b = [0] * (MAX + 1)
+
+
+# 로봇 A 이동
+start = 0
+v = 0
+for _ in range(n):
+    t, d = input().split()
+    t = int(t)
+    for i in range(start, start + t):
+        v = v + 1 if d == 'R' else v - 1
+        robot_a[i] = v
+    start += t
+
+# 로봇 B 이동
+start = 0
+v = 0
+for _ in range(n):
+    t, d = input().split()
+    t = int(t)
+    for i in range(start, start + t):
+        v = v + 1 if d == 'R' else v - 1
+        robot_b[i] = v
+    start += t
+
+# 마주치는 경우 count
+count = 0
+for i in range(1, MAX + 1):
+    if (robot_a[i - 1] != robot_b[i - 1]) and (robot_a[i] == robot_b[i]):
+        count += 1
+
+print(count)
