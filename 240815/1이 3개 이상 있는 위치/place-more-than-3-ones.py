b5cfb49e4470f0dbf803452ba17a727f83da6449
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-15 20:56:29 +0900

    Title: 1이 3개 이상 있는 위치, Time: 183ms, Memory: 26MB, Status: Passed - CodeTree

--- 240815/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240815/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -2,7 +2,7 @@ n = int(input())
 matrix = []
 
 # 동 남 서 북
-dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1]
 
 # 배열 입력
 for _ in range(n):
@@ -12,16 +12,20 @@ for _ in range(n):
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
+# 인접 1 count
+def count_adj_one(x, y):
+    local_count = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and matrix[nx][ny] == 1:
+            local_count +=1
+    return local_count
+
 # 시뮬레이션
 count = 0
 for x in range(n):
     for y in range(n):
-        local_count = 0
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if in_range(nx, ny) and matrix[nx][ny] == 1:
-                local_count +=1
-        if local_count >= 3:
+        if count_adj_one(x, y) >= 3:
             count += 1
 
 print(count)
