8c5eb598ee90673503cf0b02ba62f1f54d4f5aad
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-19 22:32:04 +0900

    Title: 체크판위에서 2, Time: 163ms, Memory: 29MB, Status: Passed - CodeTree

--- 240819/체크판위에서 2/on-the-checkboard-2.py
+++ 240819/체크판위에서 2/on-the-checkboard-2.py
@@ -1,28 +1,29 @@
+def count_ways_to_jump(r, c, matrix):
+    def dfs(x, y, jump_count, last_color):
+        # 만약 점프가 3번 일어났다면
+        if jump_count == 3:
+            # 도착 지점에 도착했는지 확인
+            return 1 if (x == r-1 and y == c-1) else 0
+        
+        # 경우의 수 카운트
+        count = 0
+        
+        # 다음 점프는 오른쪽과 아래로 최소 한 칸 이상 이동해야 함
+        for i in range(x + 1, r):
+            for j in range(y + 1, c):
+                # 색상이 달라야만 점프 가능
+                if matrix[i][j] != last_color:
+                    # 다음 점프 진행
+                    count += dfs(i, j, jump_count + 1, matrix[i][j])
+        
+        return count
+
+    # 첫 번째 점프는 시작점 (0, 0)에서 시작, 첫 번째 색은 matrix[0][0]
+    return dfs(0, 0, 0, matrix[0][0])
+
+# 입력 처리
 r, c = map(int, input().split())
-m = [
-    list(input().split())
-    for _ in range(r)
-]
+matrix = [input().split() for _ in range(r)]
 
-r, c = r - 1, c - 1
-sr, sc = 0, 0
-flag = m[sr][sc]
-
-# 탐색 시뮬레이션
-count = 0
-for i in range(sr + 1, r):
-    for j in range(sc + 1, c):
-        # 첫 번째 jump
-        if m[i][j] != flag:
-            flag = m[i][j]
-            for k in range(i + 1, r):
-                for l in range(j + 1, c):
-                    # 두 번째 jump
-                    if m[k][l] != flag:
-                        if k < r and l < c:
-                            count += 1
-                        
-if m[sr][sc] == m[r][c]:
-    print(0)
-else:
-    print(count)
+# 결과 출력
+print(count_ways_to_jump(r, c, matrix))
