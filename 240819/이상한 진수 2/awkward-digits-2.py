337f438fe8438a7e1f0430051273122b51b342ae
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-19 22:46:26 +0900

    Title: 이상한 진수 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240819/이상한 진수 2/awkward-digits-2.py
+++ 240819/이상한 진수 2/awkward-digits-2.py
@@ -1,14 +1,20 @@
 n = input()
 n = list(n)
 
+one_count = 0
 for i in range(len(n)):
     if n[i] == '0':
         n[i] = '1'
         break
+    one_count += 1
+
 n = n[:: -1]
 
 result = 0
 for i in range(len(n)):
     result += int(n[i]) * 2 ** i
 
-print(result)
+if one_count == len(n):
+    print(result -1)
+else:
+    print(result)
