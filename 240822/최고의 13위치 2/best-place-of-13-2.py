125b3170fa1de136f598bbd935b31488a17ced96
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-22 12:02:41 +0900

    Title: 최고의 13위치 2, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240822/최고의 13위치 2/best-place-of-13-2.py
+++ 240822/최고의 13위치 2/best-place-of-13-2.py
@@ -3,19 +3,28 @@ matrix = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
-sum_list = []
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 # 전역검사
+total = 0
 for i in range(n):
     for j in range(n):
         if not in_range(i, j + 2):
             continue
-        sum_list.append(matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2])
+        # 같은 열 탐색
+        for k in range(j + 1, n):
+            if not in_range(i, k + 2):
+                continue
+            total = max(total, matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2]
+                                + matrix[i][k] + matrix[i][k + 1] + matrix[i][k + 2])
+        # 다른 열 탐색
+        for k in range(i + 1, n):
+            for l in range(n):
+                if not in_range(k, l + 2):
+                    continue
+                total = max(total, matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2]
+                                    + matrix[k][l] + matrix[k][l + 1] + matrix[k][l + 2])
 
-sum_list.sort()
-first = sum_list.pop()
-second = sum_list.pop()
-print(first+ second)
+print(total)
