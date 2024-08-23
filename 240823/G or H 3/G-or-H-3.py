7f64c8cc31a648fcfd86ac0a9acae866c8c9db35
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-23 22:57:06 +0900

    Title: G or H 3, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240823/G or H 3/G-or-H-3.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+# 점수 표시 array
+array = [0] * 10001
+for _ in range(n):
+    index, alpha = input().split()
+    index = int(index)
+    # 해당 인덱스에 점수 기입
+    if alpha == 'G':
+        array[index] = 1
+    elif alpha == 'H':
+        array[index] = 2
+
+# 점수 계산
+ans = 0
+for i in range(1, n - k + 2):
+    local = 0
+    for j in range(i, i + k + 1):
+        local += array[j]
+    # 최대 점수 갱신
+    ans = max(ans, local)
+
+print(ans)
