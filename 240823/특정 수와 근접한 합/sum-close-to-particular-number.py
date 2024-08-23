b963339428b473ddd8987326c0183b5db5169f33
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-23 22:41:02 +0900

    Title: 특정 수와 근접한 합, Time: 128ms, Memory: 28MB, Status: Passed - CodeTree

--- 240823/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240823/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -5,7 +5,7 @@ total = sum(array)
 # 전역검사 수행
 ans = int(1e9)
 for i in range(n - 1):
-    for j in range(i, n):
+    for j in range(i + 1, n):
         local_sum = total - array[i] - array[j]
         ans = min(ans, abs(s - local_sum))
 
