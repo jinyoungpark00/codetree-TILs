5d8b04f087439d82cc1cd224465d82b7b27f5a08
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-24 19:34:42 +0900

    Title: 아름다운 수열 2, Time: 1297ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240824/아름다운 수열 2/beautiful-sequence-2.py
+++ 240824/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,15 +1,26 @@
-n, m = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+import itertools
 
+def count_beautiful_sequences(N, M, A, B):
+    # 수열 B의 모든 순열 생성
+    permutations_of_B = list(itertools.permutations(B))
+    
+    count = 0
+    
+    # 수열 A의 모든 길이 M인 연속 부분 수열을 추출
+    for i in range(N - M + 1):
+        sub_sequence = tuple(A[i:i+M])  # 부분 수열 추출
+        
+        # 추출된 부분 수열이 B의 순열 중 하나와 일치하는지 확인
+        if sub_sequence in permutations_of_B:
+            count += 1
+    
+    return count
 
-count = 0
-for i in range(n - m + 1):
-    is_in = 0
-    for item in b:
-        if item in a[i: i + m]:
-            is_in += 1
-    if is_in == m:
-        count += 1
+# 입력 받기
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
-print(count)
+# 결과 출력
+result = count_beautiful_sequences(N, M, A, B)
+print(result)
