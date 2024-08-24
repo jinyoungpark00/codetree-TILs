f7a713c3bfc8b4834e623c5fc4eb023f357ac830
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-24 19:54:53 +0900

    Title: 바구니 안의 사탕 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240824/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240824/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -4,11 +4,14 @@ for _ in range(n):
     candy, index = map(int, input().split())
     array[index] += candy
 
-ans = 0
-for i in range(100 - 2 * k):
-    candy_count = 0
-    for j in range(i, i + 2 * k + 1):
-        candy_count += array[j]
-    ans = max(ans, candy_count)
+if k >= 49:
+    print(sum(array))
+else:
+    ans = 0
+    for i in range(k, 100 - k + 1):
+        candy_count = 0
+        for j in range(i - k, i + k + 1):
+            candy_count += array[j]
+        ans = max(ans, candy_count)
 
-print(ans)
+    print(ans)
