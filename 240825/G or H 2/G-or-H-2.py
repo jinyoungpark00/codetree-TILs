02adf08429b27e161fcc424500fba00dd275d24a
JinYoung Park <102143229+jinyoungpark00@users.noreply.github.com>
2024-08-25 10:09:05 +0900

    Title: G or H 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240825/G or H 2/G-or-H-2.py
+++ 240825/G or H 2/G-or-H-2.py
@@ -1,31 +1,36 @@
-MAX = 100
+MAX_NUM = 100
 
+# 변수 선언 및 입력
 n = int(input())
-# 정보 입력
-index_max = 0
-array = [0] * (MAX + 1)
-for _ in range(n):
-    index, alpha = input().split()
-    index = int(index)
-    array[index] = alpha
-    index_max = max(index_max, index)
+arr = [0] * (MAX_NUM + 1)
 
-# 전역 탐색 시뮬레이션
-count = 0
-# 모든 조합의 수 탐색
-for i in range(index_max + 1):
-    for j in range(i, index_max + 1):
-        # G, H 개수 check
-        g = 0; h = 0
-        for k in range(i, j + 1):
-            if array[k] == 'G':
-                g += 1
-            elif array[k] == 'H':
-                h += 1
-        # G와 H의 개수가 같을 경우 count 갱신
-        if g == h and g > 0 and h > 0 and array[i] != 0 and array[j] != 0:
-            count = max(count, j - i)
-if array[2] == 'G' and array[4] == 'G':
-    print(2)
-else:
-    print(count)
+for _ in range(n):
+    x, c = tuple(input().split())
+    x = int(x)
+    
+    arr[x] = 1 if c == 'G' else 2
+
+# 모든 구간의 시작점을 잡아봅니다.
+max_len = 0
+for i in range(MAX_NUM + 1):
+	for j in range(i + 1, MAX_NUM + 1):
+		# i와 j 위치에 사람이 있는지 확인합니다.
+		if arr[i] == 0 or arr[j] == 0:
+			continue
+		
+		# 해당 구간 내 g와 h의 개수를 구합니다.
+		cnt_g = 0
+		cnt_h = 0
+		
+		for k in range(i, j + 1):
+			if arr[k] == 1:
+				cnt_g += 1
+			if arr[k] == 2:
+				cnt_h += 1
+		
+		# 조건을 만족할 때 구간의 길이를 구해 최댓값과 비교합니다.
+		if cnt_g == 0 or cnt_h == 0 or cnt_g == cnt_h:
+			leng = j - i
+			max_len = max(max_len, leng)
+
+print(max_len)
